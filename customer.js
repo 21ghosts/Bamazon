753af06910998b1ef9a9c474e93a1528fd4b4080
21ghosts <nathanmyoung44@gmai.com>
2018-03-01 13:06:12 -0500

    basic functioning

--- customer.js
+++ customer.js
@@ -13,16 +13,57 @@ var connection = mysql.createConnection({
 
 });
 
+var wantedAmount;// amount wanted by user
+var wantedId;// item_id wanted by user
+var price;// price of wanted item
+var buyProduct;// variable for purchase of product
+var productListings;// list of all products
+var cartPrompt;// prompt questions for user
 
-// 
-var productListing = () =>
+cartPrompt = () => {
+    inquirer.prompt([
+        {   //ask user what item they would like to buy
+            name: 'id',
+            type: 'input',
+            message: 'What is the id of the item you would like to buy?'
+            /*validate: (value) =>
+            {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }*/
+        },
+        {   //ask user what amount they would like to purchase
+            name: 'Amount',
+            type: 'input',
+            message: 'What is the amount you would like to to buy?'
+            /*validate: (value) => {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }*/
+        },
+
+    ]).then((answer) => {
+        wantedAmount = answer.Amount;
+        wantedId = answer.id;
+        buyProduct(wantedId, wantedAmount);
+    });
+
+    productListing();
+};
+
+productListing = () =>
 {
     //Conection to database
     connection.connect((err) =>
     {
         if (err) throw err;
         
-        connection.query('SELECT * FROM products', function (err, res) {
+        connection.query('SELECT * FROM products', (err, res) =>
+        {
             if (err) throw err;
             
             for (j = 0; j < res.length; j++) {
@@ -33,35 +74,34 @@ var productListing = () =>
                 console.log("_____________________")
             }
         });
-    }
-    
-    );
+        
+    });
 
-}
+};
 
-productListing();
 
-var purchasePrompt = () =>
-{
-    inquirer.prompt([
-        {
-            name: 'id',
-            type: 'input',
-            message: 'What is the id of the item you would like to buy?'
-            
-        },
-        {
-            name: 'Amount',
-            type: 'input',
-            message: 'What is the amount you wish to buy?'
-        },
-    ]).then((answer) => 
-        {
-            var amountWanted = answer.Amount;
-            var wantedId = answer.id;
 
-        })
-}
 
-purchasePrompt();
 
+
+
+buyProduct = (id, requestedAmount) => {
+    connection.query("SELECT * FROM products WHERE item_id = " + id, (err, res) => {
+        if (err) throw err;
+
+        if (requestedAmount <= res[0].stock_count) {
+            priceAmount = res[0].price * requestedAmount;
+            console.log('The items you have requested are ready.');
+            console.log('___________');
+            console.log('that will be ' + '$' + priceAmount + ' for ' + requestedAmount + ' ' + res[0].product_name + '(s)');
+            connection.query('UPDATE products SET stock_count = stock_count -' + requestedAmount + "WHERE item_id = " + id);
+            console.log(res.length)
+        }
+        else {
+            console.log('Not enough ' + res[0].product_name + ' in stock.');
+        };
+        
+    });
+};
+
+cartPrompt();
--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE products (
     item_id INT(10) AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    price DECIMAL(5,2) NOT NULL,
     stock_count INT(10) NOT NULL,
     PRIMARY KEY (item_id),
 );
